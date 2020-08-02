309602d6d807b11161dd785427219fa403fb10d7
Olivier Clero <oclero@hotmail.com>
2020-08-02 11:29:34 +0200

    Add common meta-information

---
+++ cmake/AddMetaInformation.cmake
@@ -0,0 +1,5 @@
+string(TIMESTAMP BUILD_YEAR "%Y")
+
+set(ORGANIZATION_NAME "Solarus")
+set(LEGAL_COPYRIGHT "© 2006-${BUILD_YEAR} Christopho, Solarus.")
+set(LEGAL_TRADEMARKS "Licensed under GPL v3.")
