76347fc80f98aa28c0ce89973a9984555e17988d
Alexander Schlarb <alexander@ninetailed.ninja>
2020-11-20 23:50:14 +0100

    Install all data files on XDG targets based on the configured AppID and report that value to the window system
    Based on this, the default AppID for the launcher is `org.solarus_games.solarus.Launcher`
    and `org.solarus_games.solarus.Runner` for the game runner window, but forks could easily
    set their own.

--- gui/cmake/AddInstallTargets.cmake
+++ gui/cmake/AddInstallTargets.cmake
@@ -24,41 +24,45 @@ install(FILES
 
 # FreeDesktop compatible icons
 if(UNIX AND NOT APPLE)
-  # Pixmap icons for sizes under 48x48 pixels
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_16.png
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/16x16/apps RENAME solarus-launcher.png)
+  foreach(SUFFIX IN ITEMS Launcher Runner)
+    # Pixmap icons for sizes under 48x48 pixels
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_16.png
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/16x16/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.png)
     install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_20.png
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/20x20/apps RENAME solarus-launcher.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_24.png
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/24x24/apps RENAME solarus-launcher.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_32.png
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/32x32/apps RENAME solarus-launcher.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_40.png
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/40x40/apps RENAME solarus-launcher.png)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_48.png
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/48x48/apps RENAME solarus-launcher.png)
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/20x20/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.png)
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_24.png
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/24x24/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.png)
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_32.png
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/32x32/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.png)
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_40.png
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/40x40/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.png)
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_48.png
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/48x48/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.png)
 
-  # Pixmap icon for desktop that don't support multiple sizes
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_512.png
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/pixmaps RENAME solarus-launcher.png)
+    # Pixmap icon for desktop that don't support multiple sizes
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_512.png
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/pixmaps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.png)
 
-  # Vector icons, automatically chosen for sizes above 48x48 pixels
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon.svg
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/scalable/apps RENAME solarus-launcher.svg)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_symbolic.svg
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/symbolic/apps RENAME solarus-launcher-symbolic.svg)
+    # Vector icons, automatically chosen for sizes above 48x48 pixels
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon.svg
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/scalable/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}.svg)
+    install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/images/icon/solarus_launcher_icon_symbolic.svg
+      DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/icons/hicolor/symbolic/apps RENAME ${SOLARUS_APP_ID}.${SUFFIX}-symbolic.svg)
+  endforeach()
 endif(UNIX AND NOT APPLE)
 
 # FreeDesktop compatible start menu launcher
 if(UNIX AND NOT APPLE)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/solarus-launcher.desktop
+  configure_file (resources/solarus-launcher.desktop.in resources/${SOLARUS_APP_ID}.Launcher.desktop @ONLY)
+  install (FILES ${CMAKE_CURRENT_BINARY_DIR}/resources/${SOLARUS_APP_ID}.Launcher.desktop
     DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/applications)
 endif(UNIX AND NOT APPLE)
 
 # AppStream compatible software gallery metadata
 if(UNIX AND NOT APPLE)
-  install (FILES ${CMAKE_CURRENT_SOURCE_DIR}/resources/solarus-launcher.appdata.xml
-    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/appdata)
+  configure_file (resources/solarus-launcher.appdata.xml.in resources/${SOLARUS_APP_ID}.appdata.xml @ONLY)
+  install (FILES ${CMAKE_CURRENT_BINARY_DIR}/resources/${SOLARUS_APP_ID}.appdata.xml
+    DESTINATION ${SOLARUS_SHARE_INSTALL_DESTINATION}/metainfo)
 endif(UNIX AND NOT APPLE)
 
 # Linux Manpage
