87b391f6f6cb44eab79bcf8a57071a7bde19001d
Christopho <christopho@solarus-games.org>
2021-03-03 21:21:36 +0100

    I hate this code

--- src/core/MainLoop.cpp
+++ src/core/MainLoop.cpp
@@ -504,7 +504,7 @@ void MainLoop::check_input() {
     notify_input(*event);
     event = InputEvent::get_event();
   }
-/*
+
   // Check Lua requests.
   if (!lua_commands.empty()) {
     std::lock_guard<std::mutex> lock(lua_commands_mutex);
@@ -524,7 +524,6 @@ void MainLoop::check_input() {
     }
     lua_commands.clear();
   }
-  */
 }
 
 void MainLoop::setup_game_icon() {
